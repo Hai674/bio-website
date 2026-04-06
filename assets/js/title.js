898648124fcfa64b8c72d740893429162d25c570
Hai674 <hainong2010@gmail.com>
2026-04-06 19:10:05 +0700

    0.2

--- assets/js/title.js
+++ assets/js/title.js
@@ -1,18 +1,18 @@
 var titles = [
   "@",
-  "@c",
-  "@c0",
-  "@c0m",
-  "@c0mp",
-  "@c0mpl",
-  "@c0mple",
-  "@c0mplex",
-  "@c0mple",
-  "@c0mpl",
-  "@c0mp",
-  "@c0m",
-  "@c0",
-  "@c"
+  "@H",
+  "@Ha",
+  "@Hai",
+  "@Haib",
+  "@Haibd",
+  "@Haibdo",
+  "@Haibdos",
+  "@Haibdo",
+  "@Haibd",
+  "@Haib",
+  "@Hai",
+  "@Ha",
+  "@H"
 ];
 
 function changeTitle() {

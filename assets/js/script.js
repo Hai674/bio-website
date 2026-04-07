3920cc2c5dde7ff3bc7944820f2b86cc15e32a54
Hai674 <hainong2010@gmail.com>
2026-04-07 18:11:41 +0700

    Index 0.6

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var closeButton = document.getElementById('close-button');
 
   var terminalTextContent = [
-      "User: SHUT UP NIGGA",
+      "User: Haubdos",
       "IP: Loading...",
       "System: Loading...",
       "Bio Loaded",
